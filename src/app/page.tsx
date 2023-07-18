b210bb32b271f6a1e400bee9eca9f8cf31e0b3ba
No-Cash-7970 <no-cash-7970@outlook.com>
2023-07-17 22:10:24 -0700

    chore: install Tabler Icons
    Closes #10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { IconMoodWink2 } from '@tabler/icons-react';
+
 export default function Home() {
   return (
     <main className="prose bg-base-100 max-w-none min-h-screen p-4">
-      <h1 className="text-primary text-center">Hello!</h1>
+      <h1 className="text-primary text-center">
+        <span className="align-middle">Hello!</span>
+        <IconMoodWink2 strokeWidth={1} className="inline h-10 w-10 align-middle" />
+      </h1>
     </main>
   )
 }
