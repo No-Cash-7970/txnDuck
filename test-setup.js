672aea6550174095bcc2e3b760411182c50c133b
No-Cash-7970 <No-Cash-7970@outlook.com>
2023-07-19 17:59:41 -0700

    test: add test setup script to get around error caused by module formats

---
+++ test-setup.js
@@ -0,0 +1,4 @@
+// Tell Jest to use the Chance js library through `jest-chance`
+// Use `import { chance } from 'jest-chance'` to import Chance into tests
+import jestChance from 'jest-chance';
+export default jestChance;
