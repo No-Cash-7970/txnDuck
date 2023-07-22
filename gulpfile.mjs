0962919d065e11dc5b5d206c36957238512950ed
No-Cash-7970 <no-cash-7970@outlook.com>
2023-07-21 23:21:07 -0700

    build: replace contents of `prepare` script with Gulp task in package.json
    The `prepare` script in package.json is used to build jest-chance fork (necessary because the
    original jest-chance package does not allow Node.js version < 19.0.0). Using Gulp to to build
    jest-chance provides better compatibility with Windows and gets rid of the `lefthook.yml` it
    generates every time it is built.
    NOTE: This `prepare` script for jest-chance is supposed to be temporary

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,14 +1,25 @@
 import { task } from 'gulp-execa';
 import gulp from 'gulp';
 
-const unitTest = task('yarn jest');
-const audit = task('yarn audit');
-const fail = task('yarn foo');
-const cleanup = (cb) => {
-  console.log('Cleaning up...');
-  cb();
-}
+// const unitTest = task('yarn jest');
+// const audit = task('yarn audit');
+// const fail = task('yarn foo');
+// const cleanup = (cb) => {
+//   console.log('Cleaning up...');
+//   cb();
+// }
 
-export const example_task = gulp.series(unitTest, fail, audit);
-export const go = gulp.series(task('yarn gulp example_task', { reject: false }), cleanup);
-// yarn gulp go
+// export const example_task = gulp.series(unitTest, fail, audit);
+// export const go = gulp.series(task('yarn gulp example_task', { reject: false }), cleanup);
+// // yarn gulp go
+
+// This is needed because a fork of jest-chance is being used. This task can be removed if using
+// the official jest-chance package.
+export const buildJestChance = gulp.series(
+  (cb) => {
+    process.chdir('./node_modules/jest-chance');
+    cb();
+  },
+  task('yarn install'),
+  task('yarn build'),
+);
