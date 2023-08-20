760ba53cbfe90f83fc29c806dd395e5112755857
No-Cash-7970 <no-cash-7970@outlook.com>
2023-08-19 22:21:19 -0700

    refactor(i18n): remove use of i18next instance in nav bar
    Using the i18next instance is unnecessary, but it doesn't create any problems. Removing the
    unnecessary use of the i18next instance make the code slightly cleaner.

--- src/app/[lang]/components/NavBar.tsx
+++ src/app/[lang]/components/NavBar.tsx
@@ -9,12 +9,12 @@ type Props = {
 };
 
 export default function NavBar({ lng }: Props) {
-  const { t, i18n } = useTranslation(lng || '', 'app');
+  const { t } = useTranslation(lng || '', 'app');
 
   return (
     <nav className='navbar bg-base-200 px-2 sm:px-4'>
       <a className='text-2xl font-bold font-display' href={`/${lng}`} title={t('home')}>
-        <Trans i18n={i18n} i18nKey="site_name_formatted" ns='app'>
+        <Trans i18nKey="site_name_formatted" ns='app'>
           name_pt_1<span className='text-primary'>name_pt_2</span>
         </Trans>
       </a>
