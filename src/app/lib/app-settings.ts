77cdd73d7f8244778be228ebcb0cc48aff9786f8
No-Cash-7970 <no-cash-7970@outlook.com>
2023-09-12 02:03:52 -0700

    refactor(app): gather all setting default values into one object

--- src/app/lib/app-settings.ts
+++ src/app/lib/app-settings.ts
@@ -1,7 +1,8 @@
-/** @file Collection of variable that contain the global state for app settings */
+/** @file Collection of variables that contain the global state for app settings */
 
 import { atomWithStorage } from 'jotai/utils';
 
+/** List of themes */
 export enum Themes  {
   /** Automatic. Detect user's system theme preference & use it to determine which theme to use. */
   auto = '',
@@ -11,5 +12,11 @@ export enum Themes  {
   dark = 'duck_dark',
 };
 
+/** The default values for all settings */
+export const defaults = {
+  /** Default theme: `''` (automatic) */
+  theme: Themes.auto,
+};
+
 /** Theme mode */
-export const themeAtom = atomWithStorage('theme', Themes.auto);
+export const themeAtom = atomWithStorage('theme', defaults.theme);
