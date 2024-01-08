81f18b576008b904b085ca4f0284406f449b0be2
No-Cash-7970 <no-cash-7970@outlook.com>
2024-01-07 16:04:24 -0800

    fix(compose_txn): adjust height of switch skeleton to better match actual toggle switches

--- src/app/[lang]/txn/compose/components/fields/LoadingPlaceholders/SwitchField.tsx
+++ src/app/[lang]/txn/compose/components/fields/LoadingPlaceholders/SwitchField.tsx
@@ -3,7 +3,7 @@ export default function SwitchField({ containerClass }: { containerClass?: strin
   return (
     <div className={`flex justify-between items-center ${containerClass}`}>
       <div className='skeleton rounded-md h-4 w-48'></div>
-      <div className='skeleton rounded-full h-7 w-16'></div>
+      <div className='skeleton rounded-full h-7 w-14'></div>
     </div>
   );
 }
