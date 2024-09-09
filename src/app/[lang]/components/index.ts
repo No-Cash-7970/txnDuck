b4a0ef187c75a05e118d17627efa3b44bd2b6d07
No-Cash-7970 <no-cash-7970@outlook.com>
2024-09-09 02:14:57 -0700

    refactor(app): put page-title-related components into a new folder

--- src/app/[lang]/components/index.ts
+++ src/app/[lang]/components/index.ts
@@ -2,7 +2,7 @@ export {default as BuilderSteps} from './BuilderSteps';
 export {default as Footer} from './Footer';
 export {default as JotaiProvider} from './JotaiProvider';
 export {default as NavBar} from './NavBar';
-export {default as PageTitleHeading} from './PageTitleHeading';
+export {default as PageTitleHeading} from './PageTitleHeading/PageTitleHeading';
 export {default as ToastProvider} from './ToastProvider';
 export {default as ToastViewport} from './ToastViewport';
 export {default as WalletProvider} from './wallet/WalletProvider';
