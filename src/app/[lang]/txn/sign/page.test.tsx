8e28a524b24ea85ae4ee2550411b502be08c1c76
No-Cash-7970 <no-cash-7970@outlook.com>
2024-12-17 02:51:38 -0800

    test(sign_txn): fix errors & use better test data

--- src/app/[lang]/txn/sign/page.test.tsx
+++ src/app/[lang]/txn/sign/page.test.tsx
@@ -23,7 +23,10 @@ jest.mock('next/navigation', () => ({
 // Mock the utils library because of the use of `fetch()`. This needs to be mocked because it is a
 // dependency of a child client component.
 jest.mock('../../../lib/utils.ts', () => ({
-  dataUrlToBytes: async (dataUrl: string) => new Uint8Array()
+  dataUrlToBytes: async (dataUrl: string) => new Uint8Array([
+    // eslint-disable-next-line max-len
+    130,163,115,105,103,196,64,160,45,30,164,227,212,130,37,87,3,93,161,194,63,37,179,191,45,203,242,194,252,68,41,124,98,31,205,31,64,146,186,209,7,176,68,44,15,23,205,201,247,241,24,66,239,201,233,231,168,154,207,107,77,81,180,144,116,147,151,159,140,250,14,163,116,120,110,136,163,102,101,101,205,3,232,162,102,118,206,2,202,109,170,163,103,101,110,172,116,101,115,116,110,101,116,45,118,49,46,48,162,103,104,196,32,72,99,181,24,164,179,200,78,200,16,242,45,79,16,129,203,15,113,240,89,167,172,32,222,198,47,127,112,229,9,58,34,162,108,118,206,2,202,113,146,163,114,99,118,196,32,217,118,87,219,217,132,48,63,36,98,249,84,168,29,166,173,104,128,163,46,162,59,103,201,36,56,98,0,228,113,111,150,163,115,110,100,196,32,217,118,87,219,217,132,48,63,36,98,249,84,168,29,166,173,104,128,163,46,162,59,103,201,36,56,98,0,228,113,111,150,164,116,121,112,101,163,112,97,121
+  ])
 }));
 
 // Mock use-wallet before modules that use it are imported
@@ -57,88 +60,82 @@ import SignTxnPage from './page';
 describe('Sign Transaction Page', () => {
   afterEach(() => {
     paramsMock.get.mockClear();
+    sessionStorage.clear();
   });
 
   it('has builder steps', () => {
-    render(<SignTxnPage params={{lang: ''}} />);
+    render(<SignTxnPage
+      // Create async function to convert parameters object into a Promise. The parameters must be a
+      // Promise.
+      params={(async () => ({lang: ''}))()}
+    />);
     expect(screen.getByText(/builder_steps\.sign/)).toBeInTheDocument();
   });
 
   it('has page title heading', () => {
-    render(<SignTxnPage params={{lang: ''}} />);
+    render(<SignTxnPage params={(async () => ({lang: ''}))()} />);
     expect(screen.getByRole('heading', { level: 1 })).not.toBeEmptyDOMElement();
   });
 
   it('has transaction information if there is stored transaction data', async () => {
     paramsMock.get.mockReturnValue(null);
     sessionStorage.setItem('txnData',
-      '{"txn":{"type":"pay","snd":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M",'
-      + '"fee":0.001,"fv":1,"lv":2,' // Change the fee
-      + '"rcv":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M","amt":0},'
-      + '"useSugFee":false,"useSugRounds":false,"apar_mUseSnd":false,"apar_fUseSnd":false,'
-      + '"apar_cUseSnd":false,"apar_rUseSnd":false}'
+      // eslint-disable-next-line max-len
+      '{"txn":{"type":"pay","snd":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","rcv":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","amt":0},"useSugFee":true,"useSugRounds":true,"b64Note":false,"b64Lx":false}'
     );
-    render(<SignTxnPage params={{lang: ''}} />);
+    render(<SignTxnPage params={(async () => ({lang: ''}))()} />);
     expect(await screen.findByRole('table')).toBeInTheDocument();
   });
 
-  it('has connect wallet/sign button if there is stored transaction data', () => {
+  it('has connect wallet/sign button if there is stored transaction data', async () => {
     paramsMock.get.mockReturnValue(null);
     sessionStorage.setItem('txnData',
-      '{"txn":{"type":"pay","snd":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M",'
-      + '"fee":0.001,"fv":1,"lv":2,' // Change the fee
-      + '"rcv":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M","amt":0},'
-      + '"useSugFee":false,"useSugRounds":false,"apar_mUseSnd":false,"apar_fUseSnd":false,'
-      + '"apar_cUseSnd":false,"apar_rUseSnd":false}'
+      // eslint-disable-next-line max-len
+      '{"txn":{"type":"pay","snd":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","rcv":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","amt":0},"useSugFee":true,"useSugRounds":true,"b64Note":false,"b64Lx":false}'
     );
-    render(<SignTxnPage params={{lang: ''}} />);
-    expect(screen.getByText('wallet.connect')).toBeInTheDocument();
+    render(<SignTxnPage params={(async () => ({lang: ''}))()} />);
+    expect(await screen.findByText('wallet.connect')).toBeInTheDocument();
   });
 
-  it('has file field for importing transaction if there is NO stored transaction data', () => {
+  // eslint-disable-next-line max-len
+  it('has file field for importing transaction if there is NO stored transaction data', async () => {
     paramsMock.get.mockReturnValue('');
-    render(<SignTxnPage params={{lang: ''}} />);
-    expect(screen.getByText(/import_txn.label/)).toBeInTheDocument();
+    render(<SignTxnPage params={(async () => ({lang: ''}))()} />);
+    expect(await screen.findByText(/import_txn.label/)).toBeInTheDocument();
   });
 
-  it('has "compose transaction" (back) button', () => {
+  it('has "compose transaction" (back) button', async () => {
     paramsMock.get.mockReturnValue(null);
     sessionStorage.setItem('txnData',
-      '{"txn":{"type":"pay","snd":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M",'
-      + '"fee":0.001,"fv":1,"lv":2,' // Change the fee
-      + '"rcv":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M","amt":0},'
-      + '"useSugFee":false,"useSugRounds":false,"apar_mUseSnd":false,"apar_fUseSnd":false,'
-      + '"apar_cUseSnd":false,"apar_rUseSnd":false}'
+      // eslint-disable-next-line max-len
+      '{"txn":{"type":"pay","snd":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","rcv":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","amt":0},"useSugFee":true,"useSugRounds":true,"b64Note":false,"b64Lx":false}'
     );
-    render(<SignTxnPage params={{lang: ''}} />);
-    expect(screen.getByText('sign_txn:compose_txn_btn')).toBeEnabled();
+    render(<SignTxnPage params={(async () => ({lang: ''}))()} />);
+    expect(await screen.findByText('sign_txn:compose_txn_btn')).toBeEnabled();
   });
 
-  it('has disabled "send transaction" (next step) button if transaction is NOT signed', () => {
+  // eslint-disable-next-line max-len
+  it('has disabled "send transaction" (next step) button if transaction is NOT signed', async () => {
     paramsMock.get.mockReturnValue(null);
     sessionStorage.setItem('txnData',
-      '{"txn":{"type":"pay","snd":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M",'
-      + '"fee":0.001,"fv":1,"lv":2,' // Change the fee
-      + '"rcv":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M","amt":0},'
-      + '"useSugFee":false,"useSugRounds":false,"apar_mUseSnd":false,"apar_fUseSnd":false,'
-      + '"apar_cUseSnd":false,"apar_rUseSnd":false}'
+      // eslint-disable-next-line max-len
+      '{"txn":{"type":"pay","snd":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","rcv":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","amt":0},"useSugFee":true,"useSugRounds":true,"b64Note":false,"b64Lx":false}'
     );
-    sessionStorage.removeItem('signedTxn');
-    render(<SignTxnPage params={{lang: ''}} />);
-    expect(screen.getByText('send_txn_btn')).toHaveClass('btn-disabled');
+    render(<SignTxnPage params={(async () => ({lang: ''}))()} />);
+    expect(await screen.findByText('send_txn_btn')).toHaveClass('btn-disabled');
   });
 
   it('has enabled "send transaction" (next step) button if transaction is signed', async () => {
     paramsMock.get.mockReturnValue(null);
     sessionStorage.setItem('txnData',
-      '{"txn":{"type":"pay","snd":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M",'
-      + '"fee":0.001,"fv":1,"lv":2,' // Change the fee
-      + '"rcv":"7JDB2I2R4ZXN4BAGZMRKYPZGKOTABRAG4KN2R7TWOAGMBCLUZXIMVLMA2M","amt":0},'
-      + '"useSugFee":false,"useSugRounds":false,"apar_mUseSnd":false,"apar_fUseSnd":false,'
-      + '"apar_cUseSnd":false,"apar_rUseSnd":false}'
+      // eslint-disable-next-line max-len
+      '{"txn":{"type":"pay","snd":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","rcv":"3F3FPW6ZQQYD6JDC7FKKQHNGVVUIBIZOUI5WPSJEHBRABZDRN6LOTBMFEY","amt":0},"useSugFee":true,"useSugRounds":true,"b64Note":false,"b64Lx":false}'
     );
-    sessionStorage.setItem('signedTxn', JSON.stringify('a signed transaction'));
-    render(<SignTxnPage params={{lang: ''}} />);
+    sessionStorage.setItem('signedTxn',
+      // eslint-disable-next-line max-len
+      '"data:application/octet-stream;base64,gqNzaWfEQLzVMbs6aqTbdYxSVHyTr4F30ZMvAawfKFXW9YWRQeAd+Rtrm4sXrVWpey1TV0ZExkYqawfSRiY5KWf5yrkc5AujdHhuiKNmZWXNA+iiZnbOAsptfqNnZW6sdGVzdG5ldC12MS4womdoxCBIY7UYpLPITsgQ8i1PEIHLD3HwWaesIN7GL39w5Qk6IqJsds4CynFmo3JjdsQg2XZX29mEMD8kYvlUqB2mrWiAoy6iO2fJJDhiAORxb5ajc25kxCDZdlfb2YQwPyRi+VSoHaataICjLqI7Z8kkOGIA5HFvlqR0eXBlo3BheQ=="'
+    );
+    render(<SignTxnPage params={(async () => ({lang: ''}))()} />);
     await waitFor(() => expect(screen.getByText('send_txn_btn')).not.toHaveClass('btn-disabled'));
   });
 
