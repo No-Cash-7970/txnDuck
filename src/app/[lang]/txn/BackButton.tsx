c12d0360185249a0d5e77b1b3f102c756eed62b7
No-Cash-7970 <no-cash-7970@proton.me>
2026-02-26 21:44:09 -0800

    perf(aesthetics): decrease size of icon of back button on txn-presets page

--- src/app/[lang]/txn/BackButton.tsx
+++ src/app/[lang]/txn/BackButton.tsx
@@ -16,9 +16,9 @@ export default function BackLink({ lng }: Props) {
   const { t } = useTranslation(lng || '', 'txn_presets');
   const currentURLParams = useSearchParams();
   return currentURLParams.get(txnGrpIdxParamName) !== null &&
-    <Link className='btn' href={`/${lng}/group/compose`}>
-      <IconChevronLeft aria-hidden className='rtl:hidden' />
-      <IconChevronRight aria-hidden className='hidden rtl:block' />
+    <Link className='btn align-middle leading-none' href={`/${lng}/group/compose`}>
+      <IconChevronLeft size={20} aria-hidden className='rtl:hidden' />
+      <IconChevronRight size={20} aria-hidden className='hidden rtl:block' />
       {t('back_btn')}
     </Link>;
 }
